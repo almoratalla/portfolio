274980d8248d3796b6bdb099320e479cb5985e6d
Alain Moratalla <realmoratalla@gmail.com>
2024-02-29 05:28:52 +0800

    fix: update work experience section
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -31,6 +31,25 @@ const Experiences = () => {
                 </div>
                 <div className="py-16">
                     <h4 className="alm-h4">
+                        React Developer
+                    </h4>
+                    <div className="flex flex-col justify-between md:flex-row flex-nowrap">
+                        <span className="font-semibold">Asurion</span>
+                        <span
+                            className="text-sm font-semibold"
+                            style={{ color: "#3E4C63" }}
+                        >
+                            Mar 2023 - Present
+                        </span>
+                    </div>
+                    <ul className="my-8" style={{ color: "#3E4C63" }}>
+                        <li>
+                            ▹Developed a unified client portal consisting of a core app and shared services library comprising UI components, authentication services, and other existing app services integrated into one client portal
+                        </li>
+                    </ul>
+                </div>
+                <div className="py-16">
+                    <h4 className="alm-h4">
                         Full Stack Developer | Automation
                     </h4>
                     <div className="flex flex-col justify-between md:flex-row flex-nowrap">
@@ -39,7 +58,7 @@ const Experiences = () => {
                             className="text-sm font-semibold"
                             style={{ color: "#3E4C63" }}
                         >
-                            Feb 2019 - Present
+                            Feb 2019 - Mar 2023
                         </span>
                     </div>
                     <ul className="my-8" style={{ color: "#3E4C63" }}>
